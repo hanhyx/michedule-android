8755b0c92d9d57513c9f66c2408c189c40e26ff7
hanhyxx <hanhyxx@naver.com>
2026-06-11 14:23:53 +0900

    bump version to v3.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ljh.michedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 38
-        versionName = "3.3.0"
+        versionCode = 39
+        versionName = "3.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
